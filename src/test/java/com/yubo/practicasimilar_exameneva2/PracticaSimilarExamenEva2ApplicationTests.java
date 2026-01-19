aff6daec1229d7ba1ffddc545661e532467b268b
YuboLin-GitH <yubo.lin@educa.jcyl.es>
2026-01-19 09:01:15 +0100

    todos

---
+++ src/test/java/com/yubo/practicasimilar_exameneva2/PracticaSimilarExamenEva2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yubo.practicasimilar_exameneva2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSimilarExamenEva2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
